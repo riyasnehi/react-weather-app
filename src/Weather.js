a3763a57553f76ef08b944418740b0f3455b727c
Riya Snehi <snehialia40@gmail.com>
2023-02-28 21:44:52 +0530

    icon issue resolved

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      icon: response.data.weather[0].icon,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
     });
