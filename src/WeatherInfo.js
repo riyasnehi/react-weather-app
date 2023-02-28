3cb9de548dd0955477304430d2aa9fb1b431d54f
Riya Snehi <snehialia40@gmail.com>
2023-02-28 21:37:52 +0530

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,7 +8,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <WeatherIcon code={props.data.icon} size={52} />;
+            <WeatherIcon code={props.data.icon} size={52} />
             <div className="float-left">
               <span className="temperature">
                 {Math.round(props.data.temperature)}
