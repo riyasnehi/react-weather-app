bb1f38efb549418ef9e0499677a9fa5524d14a48
Riya Snehi <snehialia40@gmail.com>
2023-03-06 15:24:57 +0530

    updated apiKey

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -35,7 +35,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    const apiKey = "b400ae3b711a616262d18b0ca2cbe78f";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
